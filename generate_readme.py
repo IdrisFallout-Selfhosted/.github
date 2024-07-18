f45879baf7531179e439bcb071b2ab1613506a2d
IdrisFallout <waithakasam2017@gmail.com>
2024-07-18 17:49:07 +0300

    use api to commit changes

--- generate_readme.py
+++ generate_readme.py
@@ -1,31 +1,22 @@
 import os
 import requests
 
-def fetch_repos():
+def fetch_repos(org):
     token = os.getenv('GITHUB_TOKEN')
-    org = os.getenv('GITHUB_ORGANIZATION')
     headers = {'Authorization': f'token {token}'}
     response = requests.get(f'https://api.github.com/orgs/{org}/repos', headers=headers)
     repos = response.json()
 
-    repo_list = []
+    markdown_table = ""
     for repo in repos:
-        repo_list.append(f'| [{repo["name"]}]({repo["html_url"]}) | {repo["description"] or "No description"} |')
+        markdown_table += f"| [{repo['name']}]({repo['html_url']}) |\n"
 
-    return repo_list
-
-def update_readme(repo_list):
-    readme_path = 'README.md'
-
-    table_header = "| Repository | Description |\n|------------|-------------|\n"
-    table_rows = "\n".join(repo_list)
-
-    with open(readme_path, 'a') as readme_file:
-        readme_file.write("\n## Organization Repositories\n")
-        readme_file.write(table_header)
-        readme_file.write(table_rows)
-        readme_file.write("\n")
+    return markdown_table
 
 if __name__ == "__main__":
-    repos = fetch_repos()
-    update_readme(repos)
+    org = os.getenv('GITHUB_ORGANIZATION')
+    markdown_content = fetch_repos(org)
+
+    with open('README.md', 'a') as f:
+        f.write("\n## Repositories\n")
+        f.write(markdown_content)
