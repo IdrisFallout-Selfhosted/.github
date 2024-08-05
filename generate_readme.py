0026cea898b5407099fa526f24a3e88f0500c961
IdrisFallout <waithakasam2017@gmail.com>
2024-08-05 19:20:15 +0300

    Update generate_readme.py

--- generate_readme.py
+++ generate_readme.py
@@ -1,40 +1,54 @@
 import os
 import requests
 
-user = os.getenv('COMMITTER_NAME')
 
-def is_contributor(repo_name, user, token, org):
-    headers = {
-        'Authorization': f'Bearer {token}',
-        'Accept': 'application/vnd.github.v3+json'
-    }
-    response = requests.get(f'https://api.github.com/repos/{org}/{repo_name}/collaborators/{user}/permission', headers=headers)
+def get_repos(org, token):
+    repos = []
+    page = 1
+    while True:
+        response = requests.get(f'https://api.github.com/orgs/{org}/repos',
+                                headers={'Authorization': f'token {token}'},
+                                params={'per_page': 100, 'page': page})
+        if response.status_code == 200:
+            repo_data = response.json()
+            if not repo_data:
+                break
+            repos.extend(repo['name'] for repo in repo_data)
+            page += 1
+        else:
+            print(f"Failed to fetch repositories. Status code: {response.status_code}")
+            print(response.text)
+            break
+    return repos
 
+
+def is_contributor(repo_name, user, org, token):
+    response = requests.get(f'https://api.github.com/repos/{org}/{repo_name}/collaborators/{user}/permission',
+                            headers={'Authorization': f'token {token}'})
     if response.status_code == 200:
         permission = response.json().get('permission', '')
         return permission in ['admin', 'write']
     return False
 
-def fetch_repo_names():
-    token = os.getenv('GITHUB_TOKEN')
+
+def main():
     org = os.getenv('GITHUB_ORGANIZATION')
-    headers = {
-        'Authorization': f'Bearer {token}',
-        'Accept': 'application/vnd.github.v3+json'
-    }
+    token = os.getenv('GITHUB_TOKEN')
+    user = os.getenv('COMMITTER_NAME')
 
-    # Fetch repositories from GitHub API
-    response = requests.get(f'https://api.github.com/orgs/{org}/repos', headers=headers)
+    if not org or not token or not user:
+        print(
+            "Error: One or more environment variables (GITHUB_ORGANIZATION, GITHUB_TOKEN, COMMITTER_NAME) are not set.")
+        return
 
-    if response.status_code == 200:
-        repos = response.json()
-        for repo in repos:
-            repo_name = repo['name']
-            if is_contributor(repo_name, user, token, org):
-                print(repo_name)
-    else:
-        print(f"Failed to fetch repositories. Status code: {response.status_code}")
-        print(response.text)
+    # Get all repos in the organization
+    repos = get_repos(org, token)
+
+    # Check contributor status for each repo
+    for repo in repos:
+        if is_contributor(repo, user, org, token):
+            print(repo)
+
 
 if __name__ == "__main__":
-    fetch_repo_names()
+    main()
